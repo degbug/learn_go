7a31762330cbce859a253bcfc10c821b2deac22a
jianhualei <jianhua.lei@ximalaya.com>
2019-09-25 22:50:57 +0800

    多语言

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -6,17 +6,25 @@ import "fmt"
 
 //常量
 const helloPrefix = "Hello, "
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+const chinese = "Chinese"
+const chineseHelloPrefix = "你好, "
 
 //主函数
 func main() {
-	fmt.Println(Hello("World"))
+	fmt.Println(Hello("World", ""))
 }
 
 // Hello 定义函数
 // 返回字符串
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
+	}
+
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 
 	return helloPrefix + name
