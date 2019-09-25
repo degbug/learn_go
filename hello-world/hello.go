7863ab004831ac42c0c5079a9e46e42016f214a8
jianhualei <jianhua.lei@ximalaya.com>
2019-09-25 20:17:34 +0800

    Hello World提交

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,11 +1,10 @@
 package main
 
+//主函数所在包必须命名为main
+//引入fmt包
 import "fmt"
 
+//主函数
 func main() {
-	fmt.Println(Hello("degbug"))
-}
-
-func Hello(name string) string {
-	return "Hello, " + name
+	fmt.Println("Hello World")
 }
