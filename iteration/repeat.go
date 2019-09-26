bceabe86b502173c77fd4cc14f94582ff2e55761
jianhualei <jianhua.lei@ximalaya.com>
2019-09-26 22:20:37 +0800

    重构Repeat函数支持传入重复次数作

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,13 +1,13 @@
 package iteration
 
 // Repeat repeat four time
-func Repeat(character string) string {
+func Repeat(character string, repeatCount int) string {
 
 	// repeated := ""
 	//定义变量
 	var repeated string
 	//循环，go只有for，没有while
-	for i := 0; i < 4; i++ {
+	for i := 0; i < repeatCount; i++ {
 		// repeated = repeated + character
 		// += 自加操作
 		repeated += character
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -6,8 +6,8 @@ import (
 )
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaa"
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
 	if repeated != expected {
 		t.Errorf("expect '%s' but got '%s'", expected, repeated)
 	}
@@ -15,15 +15,15 @@ func TestRepeat(t *testing.T) {
 
 //示例
 func ExampleRepeat() {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	fmt.Println(repeated)
-	//Output: aaaa
+	//Output: aaaaa
 }
 
 //基准测试,以Benchmark开头+要测试的函数命名
 //用go test -bench=.   来运行
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
 	}
 }
