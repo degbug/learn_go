3d885469780ffcf059f07847d04c8dc01cdaaa7f
jianhualei <jianhua.lei@ximalaya.com>
2019-09-27 23:23:25 +0800

    go 定义方法

--- perimeter/perimeter.go
+++ perimeter/perimeter.go
@@ -1,11 +1,51 @@
 package perimeter
 
-// Perimeter 计算周长
-func Perimeter(width float64, height float64) float64 {
-	return 2 * (width + height)
-}
+import "math"
+
+// // Perimeter 计算周长
+// func Perimeter(rectangle Rectangle) float64 {
+// 	return 2 * (rectangle.Height + rectangle.Width)
+// }
+
+// // Area 计算面积
+// func Area(rectangle Rectangle) float64 {
+// 	return rectangle.Width * rectangle.Height
+// }
 
 // Area 计算面积
-func Area(width float64, height float64) float64 {
-	return width * height
+// GO语言没有重载这一说
+// func Area(circle Circle) float64 {
+// 	return 2 * 3.14 * circle.Radio
+// }
+
+// Rectangle 举行对象
+// 可以用rectangle.field访问
+type Rectangle struct {
+	Height float64
+	Width  float64
+}
+
+// Area Rectangle的计算面积方法Area
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+// Perimeter Rectangle计算周长的方法
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+// Circle 圆形
+type Circle struct {
+	Radio float64
+}
+
+// Area Circle计算面积的方法
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radio * c.Radio
+}
+
+// Perimeter Circle计算周长的方法
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radio
 }
--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -1,22 +1,53 @@
 package perimeter
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
-	want := 40.0
-	if got != want {
-		//f对应float64，.2表示输出两位小数
-		t.Errorf("want '%.2f' but got '%.2f' ", want, got)
-	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+
+		got := rectangle.Perimeter()
+		want := 40.0
+		if got != want {
+			//f对应float64，.2表示输出两位小数
+			t.Errorf("want '%.2f' but got '%.2f' ", want, got)
+		}
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Perimeter()
+		fmt.Println(got)
+		if got != want {
+			//f对应float64，.2表示输出两位小数
+			t.Errorf("want '%f' but got '%f' ", want, got)
+		}
+	})
+
 }
 
 func TestArea(t *testing.T) {
-	got := Area(12.0, 6.0)
-	want := 72.0
-	if got != want {
-		t.Errorf("want '%.2f' but got '%.2f'", want, got)
-	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		got := rectangle.Area()
+		want := 72.0
+		if got != want {
+			t.Errorf("want '%.2f' but gotgit '%.2f'", want, got)
+		}
+	})
+
+	t.Run("cicles", func(t *testing.T) {
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("want '%f' but got '%f'", want, got)
+		}
+
+	})
+
 }
