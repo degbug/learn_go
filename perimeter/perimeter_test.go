aa0dd643fb461d0f2b567d6a306f8fe5a770b728
jianhualei <jianhua.lei@ximalaya.com>
2019-09-27 23:36:13 +0800

    表格驱动测试

--- perimeter/perimeter_test.go
+++ perimeter/perimeter_test.go
@@ -31,24 +31,41 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func(t *testing.T, shape Shape, want float64) {
-		t.Helper()
+	// checkArea := func(t *testing.T, shape Shape, want float64) {
+	// 	t.Helper()
 
-		got := shape.Area()
-		if got != want {
-			t.Errorf("want '%.2f' but gotgit '%.2f'", want, got)
-		}
+	// 	got := shape.Area()
+	// 	if got != want {
+	// 		t.Errorf("want '%.2f' but gotgit '%.2f'", want, got)
+	// 	}
+	// }
+
+	// t.Run("rectangles", func(t *testing.T) {
+	// 	rectangle := Rectangle{12.0, 6.0}
+	// 	checkArea(t, rectangle, 72.0)
+	// })
+
+	// t.Run("cicles", func(t *testing.T) {
+	// 	circle := Circle{10.0}
+	// 	checkArea(t, circle, 314.1592653589793)
+
+	// })
+
+	// 表格驱动测试
+
+	// 这里定义了一个匿名的结构体
+	areaTest := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{12.0, 6.0}, 72.0},
+		{Circle{10.0}, 314.1592653589793},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("cicles", func(t *testing.T) {
-		circle := Circle{10.0}
-		checkArea(t, circle, 314.1592653589793)
-
-	})
-
+	for _, tt := range areaTest {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("want '%.2f' but gotgit '%.2f'", tt.want, got)
+		}
+	}
 }
